2f976f41025ccb485fb5d29c7121eb5b2ec1af06
William Uteich <williamuteich14@gmail.com>
2024-09-30 08:53:58 -0300

    first commit

---
+++ src/app/contatos/page.tsx
@@ -0,0 +1,7 @@
+export default function Contatos(){
+    return(
+        <div>
+            contato aqui
+        </div>
+    )
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+   <main>
+    <h1>Página Home</h1>
+    <span>seja bem vindo</span>
+   </main>
+  );
+}
